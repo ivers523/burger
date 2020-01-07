a13d86cac867d46079a49eb8d22b3d4f40656cd1
ivers523 <ivers523@umn.edu>
2020-01-07 11:54:12 -0600

    added display route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,9 +6,18 @@ module.exports = function(app) {
        const burger = await db.Burger.create({name: req.body.name})
        res.json(burger);
     })
+
+    // display burgers
+    app.get("/api/burger", async function (req, res) {
+        const burger = await db.Burger.findAll({})
+        console.log(burger)
+        res.json(burger);
+    })
+
     // devour burger
     app.put("/api/burger/:id", async function (req, res) {
         const devBurger = await db.Burger.update({devoured: true},{where:{id: req.params.id}})
+        console.log(devBurger)
         res.json(devBurger);
     })
 
