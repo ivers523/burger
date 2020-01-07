b3609b7b387a288e0a7a5c47fd78deca2690000a
ivers523 <ivers523@umn.edu>
2020-01-06 21:16:28 -0600

    added scripts in public

---
+++ public/assets/scripts.js
@@ -0,0 +1,12 @@
+// Grab elements from HTML page
+
+const nameEl = document.getElementById("namebox")
+const nameBtnEl = document.getElementById("namebtn")
+const readyEl = document.getElementById("ready")
+const devouredEl = document.getElementById("devoured")
+
+// Add a new Burger
+
+
+
+// Devour Burger
