12074a67997fd8bb0622c3a3f66264ee9ff58475
ivers523 <ivers523@umn.edu>
2020-01-06 21:34:45 -0600

    changed prod config settings

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -6,7 +6,22 @@ const readyEl = document.getElementById("ready")
 const devouredEl = document.getElementById("devoured")
 
 // Add a new Burger
+nameBtnEl.addEventListener("click", function () {
+    if (nameEl.value) {
+        newBurger = nameEl.value
+        axios.post('/api/burgers', {
+            name: newBurger
+        })
+            .then(function (res) {
+                console.log(nameEl)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    } 
+})
 
+// Display added burger
 
 
 // Devour Burger
